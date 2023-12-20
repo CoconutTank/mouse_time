dfb718da26333b8413666c9b49c65a13d8ab1ddb
CoconutTank <sloser@gmail.com>
2023-12-19 23:54:13 -0800

    Comments!
    Documentation is important.

--- scripts/game_guide/guide_book.gd
+++ scripts/game_guide/guide_book.gd
@@ -2,13 +2,15 @@ class_name GuideBook
 extends Node2D
 
 
-signal guide_book_closed
-
-
+# Tracks the current page of the guide book that is being viewed.
 var guide_page_cursor = 0
+
+
+# Array containing the instantiated guide book pages.
 var guide_pages = []
 
 
+# Internal paths to the scenes for each individual page in the guide book.
 const guide_page_paths = [
 	"res://scenes/game_guide/guide_pages/game_objectives_page.tscn",
 	"res://scenes/game_guide/guide_pages/game_controls_page.tscn",
@@ -16,6 +18,9 @@ const guide_page_paths = [
 ]
 
 # Called when the node enters the scene tree for the first time.
+# There'll be a call to init_pages to try and fill in the pages. If
+# initialization is successful, the guide book will start on the first page
+# and fill in the page numbers on screen.
 func _ready():
 	init_pages()
 	if guide_pages.size() > 0:
@@ -35,6 +40,10 @@ func _process(delta):
 	pass
 
 
+# Treats each entry in guide_page_paths as a path to a page to be used in this
+# guide book. Each instantiated page is first hidden, then added to the 
+# guide pages array for tracking, and to the GuidePageDisplayLayer for
+# drawing later.
 func init_pages():
 	for page_path in guide_page_paths:
 		var guide_page = load(page_path).instantiate()
@@ -43,6 +52,8 @@ func init_pages():
 		$GuidePageDisplayLayer.add_child(guide_page)
 
 
+# Flips the guide book to the previous page. If guide_page_cursor becomes
+# less than 0, it'll wrap around to the last viable page of the guide book.
 func look_left():
 	guide_pages[guide_page_cursor].hide()
 	guide_page_cursor -= 1
@@ -51,6 +62,8 @@ func look_left():
 	$CurrentPage.text = str(guide_page_cursor + 1)
 
 
+# Flips the guide book to the next page. If guide_page_cursor exceeds the
+# available pages of the guide book, it'll wrap around to the first page.
 func look_right():
 	guide_pages[guide_page_cursor].hide()
 	guide_page_cursor += 1
@@ -59,14 +72,16 @@ func look_right():
 	$CurrentPage.text = str(guide_page_cursor + 1)
 
 
+# Function tied to the close button being pressed.
 func _on_close_button_pressed():
-	guide_book_closed.emit()
 	queue_free()
 
 
+# Function tied to the left button being pressed.
 func _on_left_button_pressed():
 	look_left()
 
 
+# Function tied to the right button being pressed.
 func _on_right_button_pressed():
 	look_right()
--- scripts/goon_gallery/goon_gallery_script.gd
+++ scripts/goon_gallery/goon_gallery_script.gd
@@ -2,13 +2,15 @@ class_name GoonGallery
 extends Node2D
 
 
-signal goon_gallery_closed
-
-
+# Tracks the current page of the goon gallery that is being viewed.
 var goon_page_cursor = 0
+
+
+# Array containing the instantiated goon gallery pages.
 var goon_pages = []
 
 
+# Internal paths to the scenes for each individual page in the goon gallery.
 const goon_page_paths = [
 	"res://scenes/goon_gallery/goon_pages/dizzy_mole_page.tscn",
 	"res://scenes/goon_gallery/goon_pages/fighty_lobster_page.tscn",
@@ -16,6 +18,9 @@ const goon_page_paths = [
 
 
 # Called when the node enters the scene tree for the first time.
+# There'll be a call to init_pages to try and fill in the pages. If
+# initialization is successful, the goon gallery will start on the first page
+# and fill in the page numbers on screen.
 func _ready():
 	init_pages()
 	if goon_pages.size() > 0:
@@ -35,6 +40,10 @@ func _process(delta):
 	pass
 
 
+# Treats each entry in goon_page_paths as a path to a page to be used in this
+# goon gallery. Each instantiated page is first hidden, then added to the 
+# goon pages array for tracking, and to the GoonPageDisplayLayer for
+# drawing later.
 func init_pages():
 	for page_path in goon_page_paths:
 		var goon_page = load(page_path).instantiate()
@@ -43,11 +52,8 @@ func init_pages():
 		$GoonPageDisplayLayer.add_child(goon_page)
 
 
-func _on_close_button_pressed():
-	goon_gallery_closed.emit()
-	queue_free()
-
-
+# Flips the goon gallery to the previous page. If goon_page_cursor becomes
+# less than 0, it'll wrap around to the last viable page of the goon gallery.
 func look_left():
 	goon_pages[goon_page_cursor].hide()
 	goon_page_cursor -= 1
@@ -56,6 +62,8 @@ func look_left():
 	$CurrentPage.text = str(goon_page_cursor + 1)
 
 
+# Flips the goon gallery to the next page. If goon_page_cursor exceeds the
+# available pages of the goon gallery, it'll wrap around to the first page.
 func look_right():
 	goon_pages[goon_page_cursor].hide()
 	goon_page_cursor += 1
@@ -64,9 +72,16 @@ func look_right():
 	$CurrentPage.text = str(goon_page_cursor + 1)
 
 
+# Function tied to the close button being pressed.
+func _on_close_button_pressed():
+	queue_free()
+
+
+# Function tied to the left button being pressed.
 func _on_left_button_pressed():
 	look_left()
 
 
+# Function tied to the right button being pressed.
 func _on_right_button_pressed():
 	look_right()
